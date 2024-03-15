00c6dc5848f9ee7107075de5974182c88ae1626b
Jhon Cesar Pablo <jhncsrpbl@gmail.com>
2024-03-15 13:25:21 +0800

    Add react-sortable-hoc and  array-move npm. Test package if working.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import { arrayMoveImmutable } from "array-move";
+
+const SortableItem = SortableElement(({ value }) => (
+  <li tabIndex={0}>{value}</li>
+));
+
+const SortableList = SortableContainer(({ items }) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${value}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+
 export default function Home() {
-  return <main className=""></main>;
+  const [items, setItems] = useState([
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6",
+  ]);
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setItems(arrayMoveImmutable(items, oldIndex, newIndex));
+  };
+
+  return <SortableList items={items} onSortEnd={onSortEnd} />;
 }
